06c8f981018eaa206be5f8a75e64d994a14a1d78
Kaushhki <kashish1105mishra@gmail.com>
2025-12-30 12:54:27 +0530

    Update student_analysis.py

--- student_analysis.py
+++ student_analysis.py
@@ -15,3 +15,11 @@ print("Lowest marks:", data["Marks"].min())
 
 if "percentage" in data.columns:
     print("Average percentage:", data["percentage"].mean())
+
+import matplotlib.pyplot as plt
+
+plt.bar(data["name"], data["Marks"])
+plt.xlabel("Students")
+plt.ylabel("Marks")
+plt.title("Student Marks Bar Chart")
+plt.show()
